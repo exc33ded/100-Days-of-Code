82d29d952e8ea596fbf035aee510781affb1c593
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-07 23:40:29 +0530

    Create band_name_generator.py

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,6 @@
+print('Hello!! Welocme to --Band Name Generator--')
+
+city = input('Tell me which which city you grew up in? ')
+pet = input('Do you have a pet or if you have one what will you name it? ')
+
+print(f'Your band name could be {pet}{city}')
