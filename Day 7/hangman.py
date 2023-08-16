98bf9929ef4bb0ef64c0cba4b9dc4bd33f933253
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-16 22:19:28 +0530

    Update hangman.py

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -6,14 +6,15 @@ chosen_word = random.choice(word_list)
 print(f'Pssst, the solution is {chosen_word}')
 guess = input("Guess a Letter: ").lower()
 
+word_length = len(chosen_word)
+
 display = []
-for _ in range(len(chosen_word)):
+for _ in range(word_length):
     display += "_"
 print(display)
 
-for position in range(len(chosen_word)):
+for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
         display[position] = letter
-
 print(display)
